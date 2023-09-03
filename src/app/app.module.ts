d60e239f9161b02c5e8d245a58744bf6b4862aa9
Jaber-Saed <92320333+Jaber-Saed@users.noreply.github.com>
2023-09-03 13:31:56 +0300

    add error page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,16 @@ import { ContactComponent } from './component/contact/contact.component';
 import { CoursesComponent } from './component/courses/courses.component';
 import { HomeComponent } from './component/home/home.component';
 import { Route, RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './component/error/error.component';
 
 const appRout: Routes = [
+  //{path:'' , component: HomeComponent} in thes case the url not be hom
   { path: '', redirectTo: 'Home', pathMatch: 'full'},
   { path: 'Home', component: HomeComponent },
   { path: 'Courses', component: CoursesComponent },
   { path: 'Contact', component: ContactComponent },
   { path: 'About', component: AboutComponent },
+  { path:  '**', component: ErrorComponent},// This will must be the last rout bucones the pathe will be mathic any rout
 ];
 
 @NgModule({
@@ -24,6 +27,7 @@ const appRout: Routes = [
     ContactComponent,
     CoursesComponent,
     HomeComponent,
+    ErrorComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRout)],
   providers: [],
