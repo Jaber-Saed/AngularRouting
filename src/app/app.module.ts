fd60d6beb44218684326b229d13386c2a95a1039
Jaber-Saed <92320333+Jaber-Saed@users.noreply.github.com>
2023-09-03 11:16:20 +0300

    new One

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './component/about/about.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { CoursesComponent } from './component/courses/courses.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      AboutComponent,
+      ContactComponent,
+      CoursesComponent,
+      HomeComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
