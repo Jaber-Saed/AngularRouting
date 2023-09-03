97fea0c37e70d4bf58cc8b0952462d70febfe344
Jaber-Saed <92320333+Jaber-Saed@users.noreply.github.com>
2023-09-03 13:15:54 +0300

    apply routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,26 @@ import { AboutComponent } from './component/about/about.component';
 import { ContactComponent } from './component/contact/contact.component';
 import { CoursesComponent } from './component/courses/courses.component';
 import { HomeComponent } from './component/home/home.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+
+const appRout: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full'; },
+  { path: 'Home', component: HomeComponent },
+  { path: 'Courses', component: CoursesComponent },
+  { path: 'Contact', component: ContactComponent },
+  { path: 'About', component: AboutComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-      AboutComponent,
-      ContactComponent,
-      CoursesComponent,
-      HomeComponent
-   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    AboutComponent,
+    ContactComponent,
+    CoursesComponent,
+    HomeComponent,
   ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRout)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
